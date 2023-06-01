eef988048654434beaff08e5db6592112fea6f82
Dawid <dawidkopicki@gmail.com>
2023-06-01 07:23:29 +0200

    refactor: moving styles inside component, code cleanup for homepage

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,15 +1,32 @@
 import { ChangeEvent, useEffect, useState } from 'react';
-import { fetchData } from './utils';
-import { Beer } from '../../types';
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Checkbox, Paper, TextField, Link } from '@mui/material';
-import styles from './Home.module.css';
-import { FAVOURITE_BEERS_KEY } from './constants'
+import { styled } from '@mui/system';
+import { fetchData } from './utils';
+import { Beer } from '../../types';
+import { FAVOURITE_BEERS_KEY } from './constants';
+
+const List = styled('ul')({
+  listStyle: 'none',
+});
+
+const ListContainer = styled('div')({
+  padding: '20px',
+  marginBottom: '20px',
+  background: '#fff',
+});
+
+const ListHeader = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingLeft: '40px',
+});
 
 const Home = () => {
   const [beerList, setBeerList] = useState<Beer[]>([]);
   const [favouriteList, setFavouriteList] = useState<{ [key: string]: Beer }>({});
-  const [filteredBeerList, setFilteredBeerList] = useState<Beer[]>(beerList)
+  const [filteredBeerList, setFilteredBeerList] = useState<Beer[]>(beerList);
   const [searchTerm, setSearchTerm] = useState<string>('');
 
   useEffect(() => {
@@ -27,24 +44,19 @@ const Home = () => {
 
   useEffect(() => {
     setFilteredBeerList(
-        beerList.filter((beer: Beer) =>
-          beer.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-      )
+      beerList.filter((beer: Beer) => beer.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    );
   }, [beerList, searchTerm]);
 
-  const handleBeerSelect = (
-    event: ChangeEvent<HTMLInputElement>,
-    beer: Beer
-  ) => {
+  const handleBeerSelect = (event: ChangeEvent<HTMLInputElement>, beer: Beer) => {
     const newFavouriteList = { ...favouriteList };
-  
+
     if (event.target.checked) {
       newFavouriteList[beer.id] = beer;
     } else {
       delete newFavouriteList[beer.id];
     }
-  
+
     setFavouriteList(newFavouriteList);
     localStorage.setItem(FAVOURITE_BEERS_KEY, JSON.stringify(newFavouriteList));
   };
@@ -64,51 +76,58 @@ const Home = () => {
     setSearchTerm('');
   };
 
-  
   return (
     <article>
       <section>
         <main>
           <Paper>
-            <div className={styles.listContainer}>
-              <div className={styles.listHeader}>
+            <ListContainer>
+              <ListHeader>
                 <TextField
-                  label='Search by name'
+                  label="Search by name"
                   value={searchTerm}
-                  onChange={(event: ChangeEvent<HTMLInputElement>) => setSearchTerm(event.target.value)}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    setSearchTerm(event.target.value)
+                  }
                 />
-                <Button variant='contained' onClick={handleReloadClick}>Reload list</Button>
-              </div>
-              <ul className={styles.list}>
-                {filteredBeerList.map((beer, index) => (
-                  <li key={index.toString()}>
-                    <Checkbox 
+                <Button variant="contained" onClick={handleReloadClick}>
+                  Reload list
+                </Button>
+              </ListHeader>
+              <List>
+                {filteredBeerList.map((beer) => (
+                  <li key={beer.id}>
+                    <Checkbox
                       checked={Object.keys(favouriteList).includes(beer.id)}
-                      onChange={(event: ChangeEvent<HTMLInputElement>) => handleBeerSelect(event, beer)}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                        handleBeerSelect(event, beer)
+                      }
                     />
                     <Link component={RouterLink} to={`/beer/${beer.id}`}>
                       {beer.name}
                     </Link>
                   </li>
                 ))}
-              </ul>
-            </div>
+              </List>
+            </ListContainer>
           </Paper>
 
           <Paper>
-            <div className={styles.listContainer}>
-              <div className={styles.listHeader}>
+            <ListContainer>
+              <ListHeader>
                 <h3>Favourite beers</h3>
                 <Button variant="contained" size="small" onClick={removeFavouriteBeers}>
                   Remove all beers
                 </Button>
-              </div>
-              <ul className={styles.list}>
-                {Object.values(favouriteList).map((beer, index) => (
-                  <li key={index.toString()}>
+              </ListHeader>
+              <List>
+                {Object.values(favouriteList).map((beer) => (
+                  <li key={beer.id}>
                     <Checkbox
                       checked={true}
-                      onChange={(event: ChangeEvent<HTMLInputElement>) => handleBeerSelect(event, beer)}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                        handleBeerSelect(event, beer)
+                      }
                     />
                     <Link component={RouterLink} to={`/beer/${beer.id}`}>
                       {beer.name}
@@ -116,8 +135,8 @@ const Home = () => {
                   </li>
                 ))}
                 {!Object.values(favouriteList).length && <p>No favourite beers</p>}
-              </ul>
-            </div>
+              </List>
+            </ListContainer>
           </Paper>
         </main>
       </section>
